0d9fe5b9562630bf5c9d2facb8c0af40c525649f
greyfoss <jueying.szu@gmail.com>
2021-12-23 08:33:10 +0800

    Implement div

--- core/ops.py
+++ core/ops.py
@@ -1,4 +1,5 @@
 from typing import Any, Tuple
+from numpy import ndarray
 
 import numpy as np
 
@@ -27,11 +28,11 @@ class _Function:
     def save_for_backward(ctx, *x: Any) -> None:
         ctx.saved_tensors.extend(x)
 
-    def forward(ctx, *args: Any, **kwargs: Any) -> np.ndarray:
+    def forward(ctx, *args: Any, **kwargs: Any) -> ndarray:
         '''前向传播，进行真正运算的地方'''
         raise NotImplementedError("You must implement the forward function for custom Function.")
 
-    def backward(ctx, grad: np.ndarray) -> np.ndarray:
+    def backward(ctx, grad: ndarray) -> Any:
         '''实现反向传播，计算梯度'''
         raise NotImplementedError("You must implement the backward method for your custom Function "
                                   "to use it with backward mode AD.")
@@ -40,7 +41,7 @@ class _Function:
         '''与PyTorch一样，我们也不直接调用forward，而是调用此方法'''
         # 先调用构造函数，传入运算依赖的Tensor
         ctx = fxn(*xs)  # 调用到了_Function的__init__方法
-        # [t.data for t in xs]遍历Tensor中的data(np.ndarray)值，参与实际计算的都是NumPy的数组。
+        # [t.data for t in xs]遍历Tensor中的data(ndarray)值，参与实际计算的都是NumPy的数组。
         ret = Tensor(ctx.forward(ctx, *[t.data for t in xs], **kwargs),
                      requires_grad=any([t.requires_grad for t in xs]))
 
@@ -50,7 +51,7 @@ class _Function:
         return ret
 
 
-def unbroadcast(grad: np.ndarray, in_shape: Tuple) -> np.ndarray:
+def unbroadcast(grad: ndarray, in_shape: Tuple) -> ndarray:
     '''
     广播操作的逆操作，确保grad转换成in_shape的形状
     Args:
@@ -77,7 +78,7 @@ def unbroadcast(grad: np.ndarray, in_shape: Tuple) -> np.ndarray:
 
 class Add(_Function):
 
-    def forward(ctx, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def forward(ctx, x: ndarray, y: ndarray) -> ndarray:
         '''
         实现 z = x + y ，我们这里的x和y都是Numpy数组，因此可能发生广播，
         在实现反向传播是需要注意
@@ -87,21 +88,21 @@ class Add(_Function):
         # 进行真正的运算
         return x + y
 
-    def backward(ctx, grad: np.ndarray) -> np.ndarray:
+    def backward(ctx, grad: ndarray) -> tuple[ndarray, ndarray]:
         shape_x, shape_y = ctx.saved_tensors
         # 输入有两个，都是需要计算梯度的，因此输出也是两个
         return unbroadcast(grad, shape_x), unbroadcast(grad, shape_y)
 
 
 class Sub(_Function):
-    def forward(ctx, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def forward(ctx, x: ndarray, y: ndarray) -> ndarray:
         '''
         实现 z = x - y
         '''
         ctx.save_for_backward(x.shape, y.shape)
         return x - y
 
-    def backward(ctx, grad: np.ndarray) -> np.ndarray:
+    def backward(ctx, grad: ndarray) -> tuple[ndarray, ndarray]:
         print(type(grad))
         shape_x, shape_y = ctx.saved_tensors
         return unbroadcast(grad, shape_x), unbroadcast(-grad, shape_y)
@@ -109,7 +110,7 @@ class Sub(_Function):
 
 class Mul(_Function):
 
-    def forward(ctx, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def forward(ctx, x: ndarray, y: ndarray) -> ndarray:
         '''
         实现 z = x * y
         '''
@@ -117,7 +118,7 @@ class Mul(_Function):
         ctx.save_for_backward(x, y)
         return x * y
 
-    def backward(ctx, grad: np.ndarray) -> np.ndarray:
+    def backward(ctx, grad: ndarray) -> tuple[ndarray, ndarray]:
         x, y = ctx.saved_tensors
         # 分别返回∂L/∂x 和 ∂L/∂y
         return unbroadcast(grad * y, x.shape), unbroadcast(grad * x, y.shape)
@@ -125,13 +126,14 @@ class Mul(_Function):
 
 class Div(_Function):
 
-    def forward(ctx, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def forward(ctx, x: ndarray, y: ndarray) -> ndarray:
         '''
         实现 z = x / y
         '''
         ctx.save_for_backward(x, y)
         return x / y
 
-    def backward(ctx, grad: np.ndarray) -> np.ndarray:
+    def backward(ctx, grad: ndarray) -> tuple[ndarray, ndarray]:
         x, y = ctx.saved_tensors
-        return unbroadcast(grad / y, x.shape), unbroadcast(grad * (-x / y ** 2), x.shape)
+        return unbroadcast(grad / y, x.shape), unbroadcast(grad * (-x / y ** 2), x.shape),
+
