402ac7c1b9cfdad9137c205450f7a9c430a8355c
nlp-greyfoss <jueying.szu@gmail.com>
2022-01-10 17:50:23 +0800

    Add sigmoid function

--- metagrad/functions.py
+++ metagrad/functions.py
@@ -1,6 +1,10 @@
 from metagrad.tensor import Tensor
 
 
+def sigmoid(x: Tensor) -> Tensor:
+    return 1 / (1 + (-x).exp())
+
+
 def binary_cross_entropy(input: Tensor, target: Tensor, reduction: str = "mean") -> Tensor:
     errors = -(target * input.log() + (1 - target) * (1 - input).log())
 
