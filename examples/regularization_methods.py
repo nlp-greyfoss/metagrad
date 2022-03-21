0d6d216e61cddf0729c3ab865c5717263e2e4e3b
greyfoss <jueying.szu@gmail.com>
2022-03-21 22:17:51 +0800

    test with l2 regularization

--- examples/regularization_methods.py
+++ examples/regularization_methods.py
@@ -37,8 +37,7 @@ class DynamicFFN(nn.Module):
         return self.net(x)
 
 
-def train_model(model, train_dl, val_dl, num_epochs=20):
-    opt = SGD(model.parameters(), lr=0.1)
+def train_model(model, opt, train_dl, val_dl, num_epochs=20):
     loss = BCELoss(reduction=None)
 
     val_losses = []
@@ -55,21 +54,58 @@ def train_model(model, train_dl, val_dl, num_epochs=20):
     return val_losses
 
 
-def compare_model(train_dl, val_dl, simple_model, complex_model, simple_label='Simple model',
-                  complex_label='Complex model'):
+def compare_model(train_dl, val_dl, original_model, new_model, original_opt, new_opt,
+                  original_label='Simple model', new_label='Complex model', ):
     num_epochs = 20
-    print(f'Training {simple_label}:')
-    original_losses = train_model(simple_model, train_dl, val_dl, num_epochs)
-    print(f'Training {complex_label}:')
-    new_losses = train_model(complex_model, train_dl, val_dl, num_epochs)
+    print(f'Training {original_label}:')
+    original_losses = train_model(original_model, original_opt, train_dl, val_dl, num_epochs)
+    print(f'Training {new_label}:')
+    new_losses = train_model(new_model, new_opt, train_dl, val_dl, num_epochs)
 
     animator = Animator(xlabel='epoch', ylabel='validation loss', yscale='log',
                         xlim=[1, num_epochs], ylim=[1e-3, 1e2],
-                        legend=[simple_label, complex_label], saved_file='animator')
+                        legend=[original_label, new_label], saved_file='animator')
     for epoch in range(num_epochs):
         animator.add(epoch + 1, (original_losses[epoch], new_losses[epoch]))
 
     animator.show()
+
+def simple_and_complex(input_size, output_size, train_dl, val_dl):
+    '''
+    比较简单模型和复杂模型
+    :param input_size:
+    :param output_size:
+    :param train_dl:
+    :param val_dl:
+    :return:
+    '''
+    simple_model = DynamicFFN(1, input_size, 4, output_size)
+    simple_opt = SGD(simple_model.parameters(), lr=0.1)
+
+    complex_model = DynamicFFN(4, input_size, 128, output_size)
+    complex_opt = SGD(simple_model.parameters(), lr=0.1)
+
+    compare_model(train_dl, val_dl, simple_model, complex_model, simple_opt, complex_opt)
+
+def complex_with_l2_or_not(input_size, output_size, train_dl, val_dl):
+    '''
+    比较有L2正则化的复杂模型和无L2正则化的复杂模型
+    :param input_size:
+    :param output_size:
+    :param train_dl:
+    :param val_dl:
+    :return:
+    '''
+    complex_model = DynamicFFN(4, input_size, 128, output_size)
+    complex_opt = SGD(complex_model.parameters(), lr=0.1)
+
+    complex_l2_model = DynamicFFN(4, input_size, 128, output_size)
+    complex_l2_opt = SGD([
+        {"params": complex_l2_model.weight, 'weight_decay': 1}, # 只有权重需要正则化
+        {"params": complex_l2_model.bias}], lr=0.1)
+
+    compare_model(train_dl, val_dl, complex_model, complex_l2_model, complex_opt, complex_l2_opt, "Complex model",
+                  "Complex Model(L2)")
 
 
 if __name__ == '__main__':
@@ -85,7 +121,8 @@ if __name__ == '__main__':
     input_size = 10000
     output_size = 1
 
-    simple_model = DynamicFFN(1, input_size, 4, output_size)
-    complex_model = DynamicFFN(4, input_size, 128, output_size)
+    complex_with_l2_or_not(input_size, output_size, train_dl, val_dl)
 
-    compare_model(train_dl, val_dl, simple_model, complex_model)
+
+
+
