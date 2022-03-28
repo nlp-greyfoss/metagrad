28b9ecf1834b4696b695222d251e31fce0fc77bc
greyfoss <jueying.szu@gmail.com>
2022-03-28 08:36:28 +0800

    test dropout

--- tests/functions/test_dropout.py
+++ tests/functions/test_dropout.py
@@ -16,10 +16,9 @@ def test_forward():
 
 def test_backward():
     x = np.ones((100, 10))
-    x = x * 5  # 每个元素值都是5
     x = Tensor(x, requires_grad=True)
-    y = F.dropout(x)  # 经过Dropout缩放后，非零元素变成了10
+    y = F.dropout(x, p=0.75)  # 经过Dropout缩放后，非零元素变成了4
     y.sum().backward()
 
-    # 反向传播时，非零元素的梯度应该是2
-    assert np.allclose(x.grad.data, y.data / 5)
+    # 反向传播时，非零元素的梯度应该是1/0.25=4
+    assert np.allclose(x.grad.data, y.data)
