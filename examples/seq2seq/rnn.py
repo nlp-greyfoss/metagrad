bf0a81d39553dfe8207eede95b5678c16a08f101
nlp-greyfoss <jueying.szu@gmail.com>
2022-09-08 09:31:17 +0800

    train seq2seq

--- examples/seq2seq/rnn.py
+++ examples/seq2seq/rnn.py
@@ -54,7 +54,7 @@ class RNNDecoder(Decoder):
         super(RNNDecoder, self).__init__(**kwargs)
         self.embedding = nn.Embedding(vocab_size, embed_size)
         # embed_size + num_hiddens 为了处理拼接后的维度，见forward函数中的注释
-        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers, batch_first=False, dropout=dropout)
+        self.rnn = nn.GRU(embed_size + num_hiddens, num_hiddens, num_layers, batch_first=False, dropout=dropout)
         # 将隐状态转换为词典大小维度
         self.dense = nn.Linear(num_hiddens, vocab_size)
 
@@ -66,13 +66,13 @@ class RNNDecoder(Decoder):
 
         # 将最顶层的上下文向量广播成与X相同的时间步，其他维度上只复制1次(保持不变)
         # 形状 (num_layers, batch_size, num_hiddens ) => (num_steps, batch_size, num_hiddens)
-        #context = state[-1].repeat((X.shape[0], 1, 1))
+        context = state[-1].repeat((X.shape[0], 1, 1))
         # 为了每个解码时间步都能看到上下文，拼接context与X
         # (num_steps, batch_size, embed_size) + (num_steps, batch_size, num_hiddens)
         #                           => (num_steps, batch_size, embed_size + num_hiddens)
-        #concat_context = F.cat((X, context), 2)
+        concat_context = F.cat((X, context), 2)
 
-        output, state = self.rnn(X, state)
+        output, state = self.rnn(concat_context, state)
         output = self.dense(output).permute((1, 0, 2))  # (batch_size, num_steps, vocab_size)
 
         return output, state
@@ -98,7 +98,7 @@ class MaskedSoftmaxCELoss(CrossEntropyLoss):
         with no_grad():
             num_tokens = label_valid.sum().item()
 
-        weighted_loss = ( unweighted_loss * label_valid ).sum() / float(num_tokens)
+        weighted_loss = (unweighted_loss * label_valid).sum() / float(num_tokens)
         return weighted_loss
 
 
@@ -114,51 +114,43 @@ def train_seq2seq(net, data_iter, lr, num_epochs, tgt_vocab, device):
     PAD_IDX = tgt_vocab[PAD_TOKEN]
 
     for epoch in tqdm(range(num_epochs)):
-        timer = Timer()
-        metric = Accumulator(2)  # 训练损失总和，单词数量
         for batch in data_iter:
             optimizer.zero_grad()
             X, Y = [x.to(device) for x in batch]
             bos = Tensor([tgt_vocab[BOS_TOKEN]] * Y.shape[0], device=device).reshape((-1, 1))
             dec_input = F.cat([bos, Y[:, :-1]], 1)  # 强制教学
             Y_hat, _ = net(X, dec_input)
-            Y = Y.view(-1)
+            Y = Y[1:].view(-1)
             output_dim = Y_hat.shape[-1]
-            Y_hat = Y_hat.view(-1, output_dim)
-            num_tokens = (Y != PAD_IDX).sum()
+            Y_hat = Y_hat[1:].view(-1, output_dim)
             l = loss(Y_hat, Y, PAD_IDX)
 
             l.backward()
             optimizer.step()
-            with no_grad():
-                metric.add(l.item(), num_tokens.item())
 
         if (epoch + 1) % 10 == 0:
-            animator.add(epoch + 1, metric[0] )
-            print(f'loss {metric[0] :.3f}, {metric[1] / timer.stop():.1f} '
-                  f'tokens/sec on {str(device)}')
+            print(f'loss {l.item() :.3f}')
 
-    print(f'loss {metric[0] :.3f}, {metric[1] / timer.stop():.1f} '
-          f'tokens/sec on {str(device)}')
+    print(f'loss {l.item() :.3f}')
     animator.show()
 
 
 # 参数定义
-embed_size = 32
-num_hiddens = 32
+embed_size = 64
+num_hiddens = 128
 num_layers = 2
 dropout = 0.1
 
-batch_size = 64
+batch_size = 128
 num_steps = 20
 
-lr = 0.005
+lr = 0.05
 num_epochs = 300
-min_freq = 2
+min_freq = 1
 device = cuda.get_device("cuda:0" if cuda.is_available() else "cpu")
 
 # 加载数据集
-train_iter, src_vocab, tgt_vocab = load_dataset_nmt('../data/en-cn/train_mini.txt', batch_size=batch_size,
+train_iter, src_vocab, tgt_vocab = load_dataset_nmt('../data/en-cn/train.txt', batch_size=batch_size,
                                                     min_freq=min_freq, max_len=num_steps)
 # 构建编码器
 encoder = RNNEncoder(len(src_vocab), embed_size, num_hiddens, num_layers, dropout)
